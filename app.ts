b56cfa8888e253252772dcdd0a498aa2c593ae00
GideonBrasil <gidxx_z@yahoo.com>
2024-03-10 21:13:34 -0600

    server get route for payload

--- app.ts
+++ app.ts
@@ -3,8 +3,17 @@ const app = express();
 
 app.use(express.json());
 
+const payload = {
+  message: "Automate all the things!",
+  timestamp: 1529729125
+};
+
 app.get('/', (req, res) => {
     res.send('Server up and runnig.');
+});
+
+app.get('/api/timestamps', (req, res) => {
+    res.json(payload)
 });
 
 const port = process.env.PORT || 3000;
