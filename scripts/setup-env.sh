0348deb4c425145ba4a82cc35ba7ce375de0eaf9
GideonBrasil <gidxx_z@yahoo.com>
2024-03-13 09:05:24 -0600

    ignore scripts for now

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -2,6 +2,26 @@
 #!/bin/bash
 # setup-env.sh
 
+set -e
+set -o pipefail
+set -x
+
+# Define your application name and Azure subscription ID
+appName="cloud_engineer"
+subscriptionId=$(az account show --query id -o tsv)
+
+# Create a Service Principal for your application
+sp=$(az ad sp create-for-rbac --name $appName --query "{clientId:appId, clientSecret:password, tenantId:tenant}" -o json)
+
+if [ $? -ne 0 ]; then
+    echo "Failed to create a Service Principal."
+    exit 1
+fi
+
+clientId=$(echo $sp | jq -r .clientId)
+clientPwd=$(echo $sp | jq -r .clientSecret)
+tenantId=$(echo $sp | jq -r .tenantId)
+
 if [ $# -eq 0 ]
 then
   echo "No .env file supplied"
@@ -12,8 +32,15 @@ set -a  # automatically export all variables
 source "$1"
 set +a
 
-chmod +x /path/to/your/scripts/setup-env.sh
+export TF_VAR_sp_password=$clientPwd
+export TF_VAR_subscription_id=$subscriptionId
+export TF_VAR_client_id=$clientId
 
-export TF_VAR_sp_password="your-secure-password"
-export TF_VAR_subscription_id="your-subscription-id"
-export TF_VAR_client_id="your-service-principal-client-id"
+echo "Environment variables set."
+echo "TF_VAR_subscription_id: $TF_VAR_subscription_id"
+echo "TF_VAR_client_id: $TF_VAR_client_id"
+echo "TF_VAR_sp_password: $TF_VAR_sp_password"
+
+# export TF_VAR_sp_password="your-secure-password"
+# export TF_VAR_subscription_id="your-subscription-id"
+# export TF_VAR_client_id="your-service-principal-client-id"
