3bba76d4dc6071651e51fae4de6dc7be306710b0
GideonBrasil <gidxx_z@yahoo.com>
2024-03-12 19:10:56 -0600

    create service principal

--- IaC/main.tf
+++ IaC/main.tf
@@ -10,3 +10,23 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_azuread_application" "aks_sp_app" {
+  name = "aks-sp-api"
+}
+
+resource "azurerm_azuread_service_principal" "aks_sp" {
+  application_id = azurerm_azuread_application.aks_app.application_id
+}
+
+resource "azurerm_azuread_service_principal_password" "aks_sp_password" {
+  service_principal_id = azurerm_azuread_service_principal.aks_sp.id
+  value                = var.sp_password
+  end_date             = "2024-03-20T00:00:000"
+}
+
+variable "sp_password" {
+  description = "Service Principal password"
+  type        = string
+  sensitive   = true
+}
