344d658462cc4fb41bf2465bd62b0323c4e5219e
GideonBrasil <gidxx_z@yahoo.com>
2024-03-23 15:39:29 -0600

    create rg and cluster outputs + create gh secrets automatically

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -5,3 +5,11 @@ output "resource_group_id" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "resource_group_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.resource_group_name
+}
