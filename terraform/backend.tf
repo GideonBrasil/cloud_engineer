cddbf12d52f0d5db3f44fbbfbe7e8b898aff3a42
GideonBrasil <gidxx_z@yahoo.com>
2024-03-28 17:59:07 -0600

    add oidc to backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name  = "pocstatewellsperformance"
     container_name        = "poctfstate"
     key                   = "env-dev/terraform.tfstate"
+    use_oidc              = true
   }
 }
