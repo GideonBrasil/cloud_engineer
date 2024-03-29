f898fcb22fdee01b30fa6e8e31d9d61d14c53a88
GideonBrasil <gidxx_z@yahoo.com>
2024-03-28 18:35:52 -0600

    remove service provider since I am using oidc + add tfvars variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,3 @@ module "aks" {
   resource_group_location = var.resource_group_location
   environment = each.key
 }
-
-module "sp" {
-  source = "./modules/sp"
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,23 +12,3 @@ variable "resource_group_location" {
   description = "Resource group location"
   type = string
 }
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Azure SP Secret"
-  type        = string
-}
