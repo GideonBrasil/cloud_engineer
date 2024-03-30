d78ce50dc6aa896b046b86ec12ea8d1d195adca0
GideonBrasil <gidxx_z@yahoo.com>
2024-03-30 11:52:59 -0600

    assign rbac admin role and improve checks | error handling

--- scripts/create-sp.sh
+++ scripts/create-sp.sh
@@ -7,17 +7,27 @@ SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 REPO=GideonBrasil/cloud_engineer
 
 # Create the Service Principal
-SP_JSON=$(az ad sp create-for-rbac --name "$SP_NAME" --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID --sdk-auth)
+SP_JSON=$(az ad sp create-for-rbac --name "$SP_NAME" --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID --sdk-auth 2>/dev/null)
+
+# Check if the Service Principal was created successfully
+if [ -z "$SP_JSON" ]; then
+    echo "Failed to create the Service Principal or retrieve the JSON output."
+    exit 1
+fi
+
+# Assign the RBAC Administrator Role to the Service Principal
+SP_OBJECT_ID=$(az ad sp list --display-name "$SP_NAME" --query "[].id" --output tsv)
+if [ -z "$SP_OBJECT_ID" ]; then
+    echo "Failed to retrieve the Service Principal's object ID."
+    exit 1
+else
+    az role assignment create --assignee $SP_OBJECT_ID --role "f58310d9-a9f6-439a-9e8d-f62e7b41a168" --scope /subscriptions/$SUBSCRIPTION_ID > /dev/null 2>&1
+fi
+
 CLIENT_ID=$(echo "$SP_JSON" | jq -r '.clientId')
 SECRET=$(echo "$SP_JSON" | jq -r '.clientSecret')
 SUBSCRIPTION_ID=$(echo "$SP_JSON" | jq -r '.subscriptionId')
 TENANT_ID=$(echo "$SP_JSON" | jq -r '.tenantId')
-
-# Check if the Service Principal was created successfully
-if [ $? -ne 0 ]; then
-    echo "Failed to create the Service Principal."
-    exit 1
-fi
 
 echo "Service Principal created successfully."
 
