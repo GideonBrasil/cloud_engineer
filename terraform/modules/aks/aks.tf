45262e92a479ea78ac44f06649297f8778a797a4
GideonBrasil <gidxx_z@yahoo.com>
2024-03-29 19:25:53 -0600

    add kube config to outputs

--- terraform/modules/aks/aks.tf
+++ terraform/modules/aks/aks.tf
@@ -21,28 +21,9 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 }
 
-# resource "kubernetes_namespace" "aks_namespace" {
-#   metadata {
-#     name = "poc-cloud-engineer-${var.environment}"
-#   }
-# }
-
 resource "azurerm_role_assignment" "aks_pull" {
   principal_id                     = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
-}
-
-resource "null_resource" "set_aks_github_secrets" {
-  depends_on = [azurerm_kubernetes_cluster.aks_cluster]
-
-  triggers = {
-    aks_cluster_name     = azurerm_kubernetes_cluster.aks_cluster.name
-    resource_group_name  = azurerm_kubernetes_cluster.aks_cluster.resource_group_name
-  }
-
-  provisioner "local-exec" {
-    command = "bash ../scripts/set_github_secrets.sh '${self.triggers.aks_cluster_name}' '${self.triggers.resource_group_name}'"
-  }
 }
--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -13,3 +13,8 @@ output "aks_cluster_name" {
 output "resource_group_name" {
   value = azurerm_kubernetes_cluster.aks_cluster.resource_group_name
 }
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  sensitive = true
+}
