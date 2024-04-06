f5d48efa33ce8aa35267438df71de18213919689
GideonBrasil <gideon.brasil@noreply.github.com>
2024-04-05 23:43:26 -0600

    change kube config to raw + format tf files
    Signed-off-by: GideonBrasil <gideon.brasil@noreply.github.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "pocTerraformStateRG"
-    storage_account_name  = "pocstatewellsperformance"
-    container_name        = "poctfstate"
-    key                   = "env-dev/terraform.tfstate"
-    use_oidc              = true
+    resource_group_name  = "pocTerraformStateRG"
+    storage_account_name = "pocstatewellsperformance"
+    container_name       = "poctfstate"
+    key                  = "env-dev/terraform.tfstate"
+    use_oidc             = true
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "environment" {
   description = "deployment environment ('dev' or 'prod')"
-  type = string
+  type        = string
 }
 
 variable "resource_group_name" {
   description = "Resource group name"
-  type = string
+  type        = string
 }
 
 variable "resource_group_location" {
   description = "Resource group location"
-  type = string
+  type        = string
 }
